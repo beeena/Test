bae5710b53cbd50309dfb9cb0dc82f77434ba9fb
mubina <mubina.ghazi@gmail.com>
2020-08-26 17:28:07 +0500

    added header

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <img alt="Vue logo" src="../assets/vaudience_logo_black.png" />
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+<style>
+img {
+  height: 40px;
+}
+</style>
